774d0cc59e3326d2d938663bd89fb423593ace08
Rameshchirumamilla <51322373+Rameshchirumamilla@users.noreply.github.com>
2021-01-09 23:12:45 +0530

    Create GreaterOf2.java

---
+++ GreaterOf2.java
@@ -0,0 +1,19 @@
+package GeekSter_assignments;
+
+import java.util.Scanner;
+
+public class GreaterOf2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a>b)
+			System.out.println(a+" is greater number");
+		else
+			System.out.println(b+" id greater number");
+
+	}
+
+}
