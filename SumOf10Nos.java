726735ec8f08f5e0350aa9b1c680063cc7aecb35
Rameshchirumamilla <51322373+Rameshchirumamilla@users.noreply.github.com>
2021-01-11 11:54:03 +0530

    Create SumOf10Nos.java

---
+++ SumOf10Nos.java
@@ -0,0 +1,14 @@
+package GeekSter_assignments;
+
+public class Natural10Nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+
+}
