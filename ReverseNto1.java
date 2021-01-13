80eaf8b1fc0b6bb89ce7fed3b4c6c4875ab4eb0e
Rameshchirumamilla <51322373+Rameshchirumamilla@users.noreply.github.com>
2021-01-13 22:20:57 +0530

    Create ReverseNto1.java

---
+++ ReverseNto1.java
@@ -0,0 +1,16 @@
+package GeekSter_assignments;
+
+import java.util.Scanner;
+
+public class ReverseNto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=n;i>=1;i--)
+			System.out.println(i);
+
+	}
+
+}
